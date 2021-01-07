3fee4c7a767fd3a16bc40565aaf90cd8b7db61c9
Henefisa <cherishnghia@gmail.com>
2021-01-08 01:32:25 +0700

    using search params to query

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,12 @@
 import { Input } from "antd";
+import { useHistory } from "react-router-dom";
 
-export default function Search({ placeholder, handleSearch }) {
+export default function Search({ placeholder, path }) {
+  const history = useHistory();
+
+  const handleSearch = values => {
+    history.push(`${path}?q=${values}`);
+  };
+
   return <Input.Search placeholder={placeholder} onSearch={handleSearch} />;
 }
--- src/pages/Playlist/index.js
+++ src/pages/Playlist/index.js
@@ -1,23 +1,33 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import Search from "../../components/Search";
 import "./index.less";
-export default function Playlist() {
-  const [playlist, setPlaylist] = useState([]);
 
-  const handleSearch = async values => {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${values}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-    );
-    const data = await response.json();
-    setPlaylist(data.items);
-  };
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+export default function Playlist() {
+  const query = useQuery();
+  const [playlist, setPlaylist] = useState([]);
+  const q = query.get("q");
+
+  useEffect(() => {
+    !!q &&
+      (async () => {
+        const response = await fetch(
+          `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${q}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+        );
+        const data = await response.json();
+        setPlaylist(data.items);
+      })();
+  }, [q]);
 
   return (
     <div className="playlist-wrapper">
       <div className="search-box">
         <h3>Search on Youtube</h3>
-        <Search placeholder="Search" handleSearch={handleSearch} />
+        <Search placeholder="Search" path="/playlist" />
       </div>
       <div className="playlist">
         {playlist.map(item => (
